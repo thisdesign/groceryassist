2f090a72eed20dedab32e8a3c5f603321582e5d0
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-19 15:09:59 -0700

    External page

---
+++ constants.tsx
@@ -0,0 +1,83 @@
+type Item = {
+  name: string;
+  count: number;
+  unit?: string;
+};
+
+type Order = {
+  id: number;
+  userId: number;
+  items: Item[];
+};
+
+type user = {
+  id: number;
+  name: string;
+  address: string;
+};
+
+// data
+
+export const USERS: user[] = [
+  {
+    name: "Adam Bagerski",
+    id: 56789876,
+    address: "4022 n albina ave portland or 97227"
+  },
+  {
+    name: "Maya Massad",
+    id: 56789877,
+    address: "4231 n interstate ave portland or 97217"
+  }
+];
+
+export const ORDERS: Order[] = [
+  {
+    id: 567890,
+    userId: 56789876,
+    items: [
+      {
+        name: "toilet paper",
+        count: 3,
+        unit: "pack"
+      },
+      {
+        name: "onion",
+        count: 3
+      }
+    ]
+  },
+  {
+    userId: 56789877,
+    id: 23434534,
+    items: [
+      {
+        name: "Hand Sanitizer",
+        count: 3
+      },
+      {
+        name: "Eggs",
+        count: 1,
+        unit: "dozen"
+      },
+      {
+        name: "Tomato sauce",
+        count: 1
+      },
+      {
+        name: "Mustard",
+        count: 1
+      },
+      {
+        name: "Salsa",
+        count: 1,
+        unit: "jar"
+      },
+      {
+        name: "Instant Oatmeal",
+        count: 1,
+        unit: "box"
+      }
+    ]
+  }
+];
