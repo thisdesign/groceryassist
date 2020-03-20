564b054f4e58a549d167669a5bae1279b59df770
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-20 14:20:10 -0700

    Set up mongoose

--- server.js
+++ server.js
@@ -3,10 +3,47 @@ const { MongoClient } = require("mongodb");
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
+const mongoose = require("mongoose");
+require("dotenv").config({});
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const PASSWORD = process.env.MONGODB_PASSWORD;
+const MONGODB_URI = `mongodb+srv://tylermcrobert:${PASSWORD}@covid-grocery-zg6ac.mongodb.net/covid-delivery?retryWrites=true&w=majority`;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+mongoose.connection.on("connected", () => {
+  console.log("connected");
+});
+
+const Schema = mongoose.Schema;
+const OrderSchema = new Schema({
+  name: String,
+  address: String,
+  date: {
+    type: String,
+    default: Date.now()
+  }
+});
+
+const submitOrder = () => {
+  const Order = mongoose.model("orders", OrderSchema);
+
+  const newOrder = new Order({
+    name: "Adam Bagerski",
+    address: "724 NE Sumner St, Portland, OR 97211"
+  });
+
+  newOrder.save(err => {
+    if (err) {
+      console.error(err);
+    }
+    console.log("success");
+  });
+};
 
 app.prepare().then(() => {
   createServer((req, res) => {
