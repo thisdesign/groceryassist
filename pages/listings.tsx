6ba412c2b876d7369d127932b64c092da2e80a21
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-20 11:18:18 -0700

    use react map package

--- pages/listings.tsx
+++ pages/listings.tsx
@@ -1,7 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { ORDERS } from "../constants";
 import { LineItem } from "../components";
 import { NextPage } from "next";
+import GoogleMapReact from "google-map-react";
+
+const GOOGLE_MAP_API_KEY = "AIzaSyBUPahFeC6Bucs95Ucc5Hf-QMO1S24nxfk";
+
+const CENTER = { lat: 45.52796, lng: -122.6964283 };
+
+const Marker: React.FC<{ lat: any; lng: any }> = ({ lat, lng }) => (
+  <>
+    <div />
+    <style jsx scoped>{`
+      div {
+        width: 1rem;
+        height: 1rem;
+        background: green;
+        border-radius: 50%;
+        border: 2px solid white;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+      }
+    `}</style>
+  </>
+);
 
 const Listings: NextPage = () => {
   return (
@@ -12,7 +33,13 @@ const Listings: NextPage = () => {
       <hr />
       <div className="split">
         <div>
-          <Map />
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: GOOGLE_MAP_API_KEY }}
+            defaultCenter={CENTER}
+            defaultZoom={8}
+          >
+            <Marker lat={CENTER.lat} lng={CENTER.lng} />
+          </GoogleMapReact>
         </div>
         <div>
           {ORDERS.map(order => (
@@ -32,35 +59,8 @@ const Listings: NextPage = () => {
           width: 50%;
         }
       `}</style>
-
-      <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUPahFeC6Bucs95Ucc5Hf-QMO1S24nxfk"
-        async
-        defer
-      ></script>
     </div>
   );
 };
 
-const Map = () => {
-  useEffect(() => {
-    const map = new (window as any).google.maps.Map(
-      document.getElementById("map"),
-      {
-        center: { lat: 45.52796, lng: -122.6964283 },
-        zoom: 12
-      }
-    );
-  }, []);
-
-  return (
-    <>
-      <div id="map">asdfasdf</div>
-      <style>{`
-        #map {
-          height: calc(100vh - 12rem);
-        }`}</style>
-    </>
-  );
-};
 export default Listings;
