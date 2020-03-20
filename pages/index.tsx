f75273ee86cb2cbcf3dc413352a40bfbe1ea3b2c
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-20 15:54:08 -0700

    unfetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,33 @@
 import Link from "next/link";
 import mongoose from "mongoose";
+import "isomorphic-unfetch";
+import { NextPage } from "next";
 
-const Home = () => (
-  <>
-    <h1>
-      {console.log(mongoose.connection)}
-      <Link href="/listings">
-        <a>I'd like to pick up groceries for someone→</a>
-      </Link>
-    </h1>
-    <hr />
-    <h1>
-      <Link href="/create">
-        <a>I'd like someone to pick up my groceries→</a>
-      </Link>
-    </h1>
+const Home: NextPage<{ data: any }> = ({ data }) => {
+  return (
+    <>
+      <h1>
+        <Link href="/listings">
+          <a>I'd like to pick up groceries for someone→</a>
+        </Link>
+      </h1>
+      <hr />
+      <h1>
+        <Link href="/create">
+          <a>I'd like someone to pick up my groceries→</a>
+        </Link>
+      </h1>
 
-    <hr />
-  </>
-);
+      <hr />
+    </>
+  );
+};
 
+Home.getInitialProps = async () => {
+  const data = await fetch("http://localhost:3000/api/getListings").then(res =>
+    res.json()
+  );
+
+  return { data };
+};
 export default Home;
