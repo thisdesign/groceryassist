ec3b9f25de28d205440fda9e88333f6040d8ddda
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-20 22:39:23 -0700

    'create' item

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Order } from "../types";
+import { Order, Item } from "../types";
+
+const ITEMS = [
+  {
+    name: "Pasta Sauce",
+    qty: 1
+  },
+  {
+    name: "Crackers",
+    qty: 1
+  },
+  {
+    name: "Spinich",
+    qty: 1
+  },
+
+  {
+    name: "cauliflower",
+    qty: 1
+  }
+];
 
 export default function App() {
   const { register, handleSubmit } = useForm();
+  const [items, setItems] = useState<Item[]>([]);
+
   const onSubmit = data => {
     const addOrder = (order: Order) => {
       fetch("http://localhost:3000/api/createOrder", {
@@ -26,68 +48,80 @@ export default function App() {
         state: data.state,
         zip: data.zip
       },
-      items: [
-        {
-          name: "Pasta Sauce",
-          qty: 1
-        },
-        {
-          name: "Crackers",
-          qty: 1
-        },
-        {
-          name: "Spinich",
-          qty: 1
-        },
-
-        {
-          name: "cauliflower",
-          qty: 1
-        }
-      ]
+      items: ITEMS
     });
   };
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="First name"
-        name="first"
-        ref={register({ required: true, maxLength: 80 })}
-      />
-      <input
-        type="text"
-        placeholder="Last name"
-        name="last"
-        ref={register({ required: true, maxLength: 100 })}
-      />
-      <input
-        type="text"
-        placeholder="Address"
-        name="address"
-        ref={register({ required: true })}
-      />
-      <input
-        type="text"
-        placeholder="State"
-        name="state"
-        ref={register({ required: true })}
-      />
-      <input
-        type="text"
-        placeholder="City"
-        name="city"
-        ref={register({ required: true })}
-      />
-      <input
-        type="number"
-        placeholder="Zip"
-        name="zip"
-        ref={register({ required: true, min: 4 })}
-      />
+  const handleNewItem = (e: any) => {
+    e.preventDefault();
+    console.log(e);
+  };
 
-      <input type="submit" />
-    </form>
+  return (
+    <div>
+      <h2>Choose your items</h2>
+      <form onSubmit={handleNewItem}>
+        <input type="text" />
+        <button type="submit">Add Item</button>
+      </form>
+      <hr />
+      <h2>Where should we deliver?</h2>
+      <p>Your information will remain private</p>
+      <br />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="First name"
+          name="first"
+          ref={register({ required: true, maxLength: 80 })}
+        />
+        <input
+          type="text"
+          placeholder="Last name"
+          name="last"
+          ref={register({ required: true, maxLength: 100 })}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Address"
+          name="address"
+          ref={register({ required: true })}
+        />
+
+        <br />
+
+        <input
+          type="text"
+          placeholder="State"
+          name="state"
+          ref={register({ required: true })}
+        />
+        <input
+          type="text"
+          placeholder="City"
+          name="city"
+          ref={register({ required: true })}
+        />
+        <input
+          type="number"
+          placeholder="Zip"
+          name="zip"
+          ref={register({ required: true, min: 4 })}
+        />
+
+        <br />
+
+        <hr />
+        {items.map(item => (
+          <div>
+            {item.name} ( - {item.qty} + )
+            <hr />
+          </div>
+        ))}
+
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
