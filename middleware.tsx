b0a3004b5b791a57a3074cfec9a7f216d7124da8
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-21 19:13:27 -0700

    Update api route conditional

--- middleware.tsx
+++ middleware.tsx
@@ -1,7 +1,10 @@
 import { OrderDb, OrderRes, Order } from "./types";
 import "isomorphic-unfetch";
 
-const apiRoute = "http://localhost:3000/api";
+const isDev = process.env.NODE_ENV !== "production";
+const apiRoute = isDev
+  ? "http://localhost:3000/api"
+  : "https://covid-delivery.now.sh/";
 
 export const getOrderById = async (id: string): Promise<OrderDb> =>
   fetch(`${apiRoute}/order?id=${id}`).then(res => res.json());
