18554291ca98fbfbd3c8fe60a5dd510e25bee55c
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-21 21:09:51 -0700

    Fix middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,10 +1,10 @@
-import { OrderDb, OrderRes, Order } from "./types";
 import "isomorphic-unfetch";
+import { OrderDb, OrderRes, Order } from "./types";
 
 const isDev = process.env.NODE_ENV !== "production";
 const apiRoute = isDev
   ? "http://localhost:3000/api"
-  : "https://covid-delivery.now.sh/";
+  : "https://covid-delivery.now.sh/api/";
 
 export const getOrderById = async (id: string): Promise<OrderDb> =>
   fetch(`${apiRoute}/order?id=${id}`).then(res => res.json());
@@ -13,7 +13,7 @@ export const getOrders = (): Promise<OrderRes> =>
   fetch(`${apiRoute}/orders`).then(res => res.json());
 
 export const addOrder = (order: Order) => {
-  fetch("http://localhost:3000/api/createOrder", {
+  fetch(`${apiRoute}/createOrder`, {
     method: "POST",
     body: JSON.stringify(order)
   });
