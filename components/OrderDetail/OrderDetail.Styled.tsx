deac624bf0e309db340db20be7324106f26bfc78
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-25 15:10:31 -0700

    sticky to fixed

--- components/OrderDetail/OrderDetail.Styled.tsx
+++ components/OrderDetail/OrderDetail.Styled.tsx
@@ -39,7 +39,8 @@ const Item = styled.div`
 `
 
 const BottomBar = styled.div`
-  position: sticky;
+  position: fixed;
+  width: 100%;
   bottom: 0;
   box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.05);
   border-top: 1px solid var(--color-grey-2);
