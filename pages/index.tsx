78d5887cc6a50bf642cd501c46b81e2e2080159b
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-25 14:14:44 -0700

    add address field to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
 import Link from "next/link"
 import "isomorphic-unfetch"
 import { NextPage } from "next"
-import { DEFAULT_CORDS } from "../constants"
 
 const Home: NextPage = () => {
+  const [address, setAddress] = useState<string>("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value)
+  }
+
+  const urlAddress = address.replace(/ /g, "+")
+
   return (
     <>
       <h1>
@@ -11,33 +19,71 @@ const Home: NextPage = () => {
         COVID-19
       </h1>
       <div className="wrapper">
-        <h2>
-          <Link href="/orders?a=4231+n+interstate+ave,+Portland,+OR,+97217">
-            <a>I&apos;d like to pick up groceries for someone→</a>
-          </Link>
-        </h2>
-        <h2>
-          <Link href="/new">
-            <a>I&apos;d like someone to pick up my groceries→</a>
-          </Link>
-        </h2>
+        <div>
+          <form>
+            <input
+              type="text"
+              onChange={handleChange}
+              value={address}
+              placeholder="Enter address"
+            />
+          </form>
+        </div>
+
+        <Link href={`/new?a=${urlAddress}`}>
+          <a>
+            <div className="button">Have groceries delivered</div>
+          </a>
+        </Link>
+
+        <Link href={`/orders?a=${urlAddress}`}>
+          <a>
+            <div className="button black">Pick up groceries for someone</div>
+          </a>
+        </Link>
       </div>
 
-      <hr />
+      <br />
+      <br />
       <style jsx>
         {`
-          h1 {
-            font-size: 4rem;
-            max-width: 15em;
+          .wrapper {
             text-align: center;
-            margin: 2em auto;
           }
 
-          .wrapper {
-            justify-content: space-around;
-            display: grid;
-            grid-template-columns: 20rem 20rem;
+          h1 {
+            font-size: var(--size-6);
+            max-width: 15em;
             text-align: center;
+            margin: var(--size-10) auto;
+          }
+
+          .button {
+            max-width: 10rem;
+            text-align: center;
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+            display: inline-block;
+            margin: 0.25rem;
+            font-size: var(--size-1);
+            background: var(--color-grey-1);
+            color: var(--bg-color);
+          }
+          .black {
+            background: var(--color-primary);
+          }
+
+          input {
+            font-size: 1rem;
+            padding: 0.5rem var(--size-0);
+            border: 1px solid var(--color-grey-2);
+            color: var(--color-primary);
+            border-radius: 0.5rem;
+            margin: 0;
+            width: 100%;
+            max-width: 25rem;
+            margin: 0 auto;
+            outline: none;
           }
         `}
       </style>
