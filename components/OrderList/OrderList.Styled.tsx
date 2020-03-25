47df57fa1f9f67b4566d65d90f9b938aba9bf0fc
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-24 21:33:27 -0700

    Responsive order list

--- components/OrderList/OrderList.Styled.tsx
+++ components/OrderList/OrderList.Styled.tsx
@@ -1,24 +1,32 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import mq from "../../style/mq"
 
 const OrderList = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-  height: 100%;
+  @media ${mq.sm} {
+    display: flex;
+    flex-direction: row-reverse;
+    height: 100%;
 
-  & > div {
-    width: 50%;
+    & > div {
+      width: 50%;
 
-    &:nth-child(2) {
-      max-width: 25rem;
-      border-right: 1px solid var(--color-grey-2);
-    }
+      &:nth-child(2) {
+        max-width: 25rem;
+        border-right: 1px solid var(--color-grey-2);
+      }
 
-    &:nth-child(1) {
-      flex: 1;
+      &:nth-child(1) {
+        flex: 1;
+      }
     }
   }
-`;
+  & > div {
+    &:nth-child(1) {
+      height: 250px;
+    }
+  }
+`
 
 export default {
   OrderList
-};
+}
