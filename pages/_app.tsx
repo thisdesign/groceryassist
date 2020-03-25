e3dc646e9a18039999758a007c0476bc1f0e7cc0
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-24 17:35:03 -0700

    add styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,61 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../style/GlobalStyle";
+import theme from "../style/theme";
 
 const App = ({ pageProps, Component }) => {
   return (
-    <div>
-      <Link href="/">
-        <a>GROCERY APP</a>
-      </Link>
-      <hr />
-
-      <Component {...pageProps} />
-      <style global jsx>
-        {`
-          html,
-          body {
-            font-family: helvetica, arial, sans-serif;
-            font-size: 18px;
-          }
-
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6,
-          p {
-            margin: 0;
-          }
-
-          h1 {
-            font-size: 2.75rem;
-          }
-          h2 {
-            font-size: 2rem;
-          }
-          h3 {
-            font-size: 1.25rem;
-          }
-          h4 {
-            font-size: 1.125rem;
-          }
-          h5 {
-            font-size: 1rem;
-          }
-
-          hr {
-            border: 0;
-            border-bottom: 1px dashed black;
-          }
-
-          a {
-            color: black;
-            text-decoration: none;
-          }
-        `}
-      </style>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div>
+        <Link href="/">
+          <a>Grocery App</a>
+        </Link>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   );
 };
 
---
+++ style/GlobalStyle.tsx
@@ -0,0 +1,85 @@
+import { createGlobalStyle, css } from "styled-components";
+import reset from "styled-reset";
+import modularScale from "./modularScale";
+import { mq } from "./index";
+
+const scale: number[] = modularScale({
+  scale: 1.333,
+  stepsDown: 2,
+  length: 15
+});
+
+const style = css`
+  :root {
+    --bg-color: white;
+    --color-primary: #101011;
+    --color-grey-1: #8f9398
+      ${scale.map((item, i) => `--size-${i}: ${item}rem`).join(";\n")};
+    --spacing-standard: var(--size-0);
+  }
+
+  @font-face {
+    font-family: "Basis Grotesque";
+    src: url("/fonts/basis-grotesque-medium.otf");
+  }
+
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    box-sizing: border-box;
+  }
+
+  body,
+  html {
+    line-height: ${props => props.theme.lineHeights.body};
+    font-family: ${props => props.theme.fontFamilies.sansSerif};
+    letter-spacing: 0.02em;
+    color: var(--color-primary);
+    background: var(--bg-color);
+    font-size: 18px;
+    overscroll-behavior-y: none;
+
+    @media ${mq.xs} {
+      font-size: 19px;
+    }
+    @media ${mq.sm} {
+      font-size: 20px;
+    }
+    @media ${mq.md} {
+      font-size: 21px;
+    }
+    @media ${mq.lg} {
+      font-size: 1.3vw;
+    }
+  }
+
+  a {
+    color: var(--text-color);
+    text-decoration: none;
+  }
+
+  em {
+    font-style: italic;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: ${props => props.theme.lineHeights.heading};
+  }
+
+  img,
+  video {
+    display: block;
+    width: 100%;
+  }
+`;
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  ${style}
+`;
+
+export default GlobalStyle;
