cb330baa6f4bd2a7fd51b4d09bcb5b9b278ef9f6
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-27 19:27:04 -0700

    shim bottom bar

--- components/BottomBar/BottomBar.tsx
+++ components/BottomBar/BottomBar.tsx
@@ -1,15 +1,32 @@
 import styled from "styled-components"
 
-const BottomBar = styled.div`
+const HEIGHT = `var(--size-9)`
+const BottomBarStyle = styled.div`
   position: fixed;
   width: 100%;
   bottom: 0;
   box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.05);
   border-top: 1px solid var(--color-grey-2);
-  padding: 0.5rem var(--size-1);
   z-index: 20;
   background: var(--bg-color);
   text-align: right;
+  grid-template-columns: 1fr;
+  align-items: center;
+  display: grid;
+  height: ${HEIGHT};
 `
+
+const Shim = styled.div`
+  height: ${HEIGHT};
+`
+
+const BottomBar: React.FC = props => {
+  return (
+    <>
+      <BottomBarStyle {...props} />
+      <Shim />
+    </>
+  )
+}
 
 export default BottomBar
