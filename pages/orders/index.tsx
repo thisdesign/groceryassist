3757a69eb5af63eb2038b10345edef5f8d9df021
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-28 18:30:28 -0700

    store cookie

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,10 +1,11 @@
 import { NextPage, GetServerSideProps } from "next"
+import Cookies from "js-cookie"
+import cookie from "cookie"
 import Router from "next/router"
 import { OrderList, PhoneCapture, AddressCapture } from "../../components"
 import { OrderRes, LocationRes } from "../../types"
 import "isomorphic-unfetch"
 import { getOrders, getLocationByAddress } from "../../middleware"
-import formatLocation from "../../util/formatLocation"
 import spaceToPlus from "../../util/spaceToPlus"
 
 const goToOrdersPage = (address: string) =>
@@ -18,18 +19,28 @@ const Listings: NextPage<{
     return <OrderList orders={orders} location={location} />
   }
 
-  return <AddressCapture onSubmit={address => goToOrdersPage(address)} />
+  return (
+    <AddressCapture
+      onSubmit={address => {
+        Cookies.set("_address", address)
+        goToOrdersPage(address)
+      }}
+    />
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
   res,
+  req,
   query
 }) => {
   let orders = null
   let location: LocationRes = null
 
-  if (query.a) {
-    const address = query.a.toString()
+  const { _address: addressCookie } = cookie.parse(req.headers.cookie)
+
+  if (query.a || addressCookie) {
+    const address = query.a ? query.a.toString() : addressCookie
     location = await getLocationByAddress(address)
 
     orders = await getOrders({
