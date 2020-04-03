95260a6793b70439fa95ee5bf1b56f418a9e6d5f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-03 10:44:18 -0700

    update infoprompt

--- components/OrderInfoPrompt/OrderInfoPrompt.Styled.tsx
+++ components/OrderInfoPrompt/OrderInfoPrompt.Styled.tsx
@@ -1,16 +1,17 @@
 import styled from "styled-components"
+import { UIWrapper } from "components"
 
-const Wrap = styled.div`
-  max-width: 30rem;
+const Wrap = styled(UIWrapper)`
   margin: var(--size-8) auto;
   padding: var(--size-2);
 `
 
 const Grid = styled.div`
+  max-width: 30rem;
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   grid-gap: var(--size-0);
-  margin: var(--size-3) 0 var(--size-7);
+  margin: var(--size-3) 0 var(--size-9);
 
   > * {
     grid-column: span 2;
--- components/OrderInfoPrompt/OrderInfoPrompt.tsx
+++ components/OrderInfoPrompt/OrderInfoPrompt.tsx
@@ -1,8 +1,17 @@
-import { UIButton, TextArea, TextInput, AddressInput } from "components"
+/* eslint-disable react/jsx-wrap-multilines */
+import {
+  UIButton,
+  TextArea,
+  TextInput,
+  AddressInput,
+  Paragraph,
+  AppFrame,
+  LargeHeading,
+  MediumHeading
+} from "components"
 import { Item } from "types"
 import { useForm } from "react-hook-form"
-import Paragraph from "components/Paragraph/Paragraph"
-import { addOrder } from "middleware"
+
 import Router from "next/router"
 import S from "./OrderInfoPrompt.Styled"
 
@@ -21,93 +30,105 @@ const OrderInfo: React.FC = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <S.Wrap>
-        Your contact information
-        <Paragraph>We only list your first name</Paragraph>
-        <S.Grid>
-          <TextInput
-            style={{ gridColumn: "span 3" }}
-            type="text"
-            placeholder="First name"
-            name="first"
-            ref={register({ required: true, maxLength: 80 })}
-          />
-          <TextInput
-            style={{ gridColumn: "span 3" }}
-            type="text"
-            placeholder="Last name"
-            name="last"
-            ref={register({ required: true, maxLength: 100 })}
-          />
-          <TextInput
-            type="tel"
-            placeholder="Phone number"
-            name="phone"
-            ref={register({ required: true, min: 10, maxLength: 13 })}
-          />
+    <AppFrame
+      header={
+        <>
+          <MediumHeading>Complete Order</MediumHeading>
+          <Paragraph>
+            Fill out some additional details so
+            <br /> we can get your order to you.
+          </Paragraph>
+        </>
+      }
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <S.Wrap>
+          Your contact information
+          <Paragraph>We only list your first name</Paragraph>
+          <S.Grid>
+            <TextInput
+              style={{ gridColumn: "span 3" }}
+              type="text"
+              placeholder="First name"
+              name="first"
+              ref={register({ required: true, maxLength: 80 })}
+            />
+            <TextInput
+              style={{ gridColumn: "span 3" }}
+              type="text"
+              placeholder="Last name"
+              name="last"
+              ref={register({ required: true, maxLength: 100 })}
+            />
+            <TextInput
+              type="tel"
+              placeholder="Phone number"
+              name="phone"
+              ref={register({ required: true, min: 10, maxLength: 13 })}
+            />
 
-          <TextInput
-            style={{ gridColumn: "span 1" }}
-            type="text"
-            placeholder="Age"
-            name="age"
-            ref={register({ required: true })}
-          />
-        </S.Grid>
-        Where should we deliver?
-        <Paragraph>You will be paired with nearby volunteers</Paragraph>
-        <S.Grid>
-          <TextInput
-            style={{ gridColumn: "span 4" }}
-            type="text"
-            placeholder="Address"
-            name="address"
-            ref={register({ required: true, min: 9 })}
-          />
-          <TextInput
-            type="text"
-            placeholder="City"
-            name="city"
-            ref={register({ required: true, min: 3 })}
-          />
-          <TextInput
-            style={{ gridColumn: "span 1" }}
-            type="text"
-            placeholder="State"
-            name="state"
-            ref={register({ required: true, min: 3 })}
-          />
+            <TextInput
+              style={{ gridColumn: "span 1" }}
+              type="text"
+              placeholder="Age"
+              name="age"
+              ref={register({ required: true })}
+            />
+          </S.Grid>
+          Where should we deliver?
+          <Paragraph>You will be paired with nearby volunteers</Paragraph>
+          <S.Grid>
+            <TextInput
+              style={{ gridColumn: "span 4" }}
+              type="text"
+              placeholder="Address"
+              name="address"
+              ref={register({ required: true, min: 9 })}
+            />
+            <TextInput
+              type="text"
+              placeholder="City"
+              name="city"
+              ref={register({ required: true, min: 3 })}
+            />
+            <TextInput
+              style={{ gridColumn: "span 1" }}
+              type="text"
+              placeholder="State"
+              name="state"
+              ref={register({ required: true, min: 3 })}
+            />
 
-          <TextInput
-            type="number"
-            placeholder="Zip"
-            name="zip"
-            ref={register({ required: true, min: 5 })}
-          />
-          <TextInput
-            type="text"
-            placeholder="Apt, suite. (optional)"
-            name="apt"
-            ref={register({ required: false })}
-          />
-        </S.Grid>
-        Where should we deliver?
-        <Paragraph>
-          Are you gluten free? Where would you like us to deliver?
-        </Paragraph>
-        <S.Grid>
-          <TextArea
-            style={{ gridColumn: "span 1" }}
-            placeholder="Order Notes (optional)"
-            name="orderNotes"
-            rows={4}
-            ref={register}
-          />
-        </S.Grid>
-        <UIButton type="submit">Submit</UIButton>
-      </S.Wrap>
-    </form>
+            <TextInput
+              type="number"
+              placeholder="Zip"
+              name="zip"
+              ref={register({ required: true, min: 5 })}
+            />
+            <TextInput
+              type="text"
+              placeholder="Apt, suite. (optional)"
+              name="apt"
+              ref={register({ required: false })}
+            />
+          </S.Grid>
+          Where should we deliver?
+          <Paragraph>
+            Are you gluten free? Where would you like us to deliver?
+          </Paragraph>
+          <S.Grid>
+            <TextArea
+              style={{ gridColumn: "span 1" }}
+              placeholder="Order Notes (optional)"
+              name="orderNotes"
+              rows={4}
+              ref={register}
+            />
+          </S.Grid>
+          <UIButton type="submit">Submit</UIButton>
+        </S.Wrap>
+      </form>
+    </AppFrame>
   )
 }
 
