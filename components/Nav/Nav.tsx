6a369bdb75585697a12dbc87ea915141dfac5b40
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-08 09:51:27 -0700

    remove floating console log

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -3,8 +3,6 @@ import Link from "next/link"
 import S from "./Nav.Styled"
 
 const Nav: React.FC<{ floating?: boolean }> = ({ floating }) => {
-  console.log(floating)
-
   return (
     <S.Nav floating={floating}>
       <Link href="/">
