ded6dc3938d3800e8381128b6f01684a524d1a90
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-09 11:21:22 -0700

    add title

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,9 +1,13 @@
 import React from "react"
 import { NextPage } from "next"
-import { Privacy as PrivacyComponent } from "components"
+import { Privacy as PrivacyComponent, Page } from "components"
 
 const Privacy: NextPage = () => {
-  return <PrivacyComponent />
+  return (
+    <Page title="Privacy">
+      <PrivacyComponent />
+    </Page>
+  )
 }
 
 export default Privacy
