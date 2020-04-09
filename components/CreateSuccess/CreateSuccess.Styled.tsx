3b42f556a322b3aca04ecb520244edea29a7ef07
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-09 15:51:54 -0700

    submission

---
+++ components/CreateSuccess/CreateSuccess.Styled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  max-width: 25rem;
+  text-align: center;
+  margin: 0 auto;
+
+  > * {
+    margin-bottom: 1rem;
+  }
+`
+
+export default {
+  Wrapper,
+}
--- components/CreateSuccess/CreateSuccess.tsx
+++ components/CreateSuccess/CreateSuccess.tsx
@@ -1,25 +1,32 @@
 import React from "react"
-import { UIWrapper, LargeHeading, Paragraph, UIButton } from "components"
+import {
+  UIWrapper,
+  LargeHeading,
+  Paragraph,
+  UIButton,
+  LargeMessage,
+} from "components"
+import S from "./CreateSuccess.Styled"
 
 const CreateSuccess = () => {
   return (
-    <div>
+    <LargeMessage>
       <UIWrapper pad>
-        <LargeHeading>Your request has been submitted</LargeHeading>
-        <div>
-          Remember, this service is run on basic human compassion and kindness.
-          Please honor the system and be patient. Together, we'll get through
-          this.
-        </div>
-
-        <Paragraph>
-          Please note that delivery cannot be 100% guaranteed. If you
-          haven&apos;t gotten a response in a few days, you may need to find
-          alternative means for having your groceries delivered to you.
-        </Paragraph>
-        <UIButton>Go Home</UIButton>
+        <S.Wrapper>
+          <LargeHeading>Your request has been submitted</LargeHeading>
+          <div>
+            Remember, this service is run on basic human compassion and
+            kindness. Please honor the system and be patient. Together, we'll
+            get through this.
+          </div>
+          <div>
+            Please note that delivery cannot be 100% guaranteed. If you
+            haven&apos;t gotten a response in a few days, you may need to find
+            alternative means for having your groceries delivered to you.
+          </div>
+        </S.Wrapper>
       </UIWrapper>
-    </div>
+    </LargeMessage>
   )
 }
 
